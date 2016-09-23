2f18f5653971c0cbec1b970e59967984c2f55680
Leon Shi <superRaytin@gmail.com>
2016-09-23 15:14:07 +0800

    docs: update controlled & uncontrolled example

--- examples/index.js
+++ examples/index.js
@@ -2,40 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import MonacoEditor from 'react-monaco-editor';
 
-// wrapper
-class SampleEditor extends React.Component {
-  onDidMount(editor) {
-    const { onDidMount } = this.props;
-    if (onDidMount) {
-      onDidMount(editor);
-    }
-  }
-  onChange(newValue, e) {
-    const { onChange } = this.props;
-    if (onChange) {
-      onChange(newValue, e);
-    }
-  }
-  render() {
-    const { width, height, language, value, requireConfig, options } = this.props;
-    const finalProps = {
-      width,
-      height,
-      language,
-      value,
-      requireConfig,
-      options,
-    };
-    return (
-        <MonacoEditor
-            {...finalProps}
-            onChange={::this.onChange}
-            onDidMount={::this.onDidMount}
-        />
-    );
-  }
-}
-
 // Using with webpack
 class CodeEditor extends React.Component {
   constructor(props) {
@@ -45,7 +11,7 @@ class CodeEditor extends React.Component {
     }
   }
   onDidMount(editor) {
-    console.log('onDidMount', editor, editor.model, editor.getValue(), editor.getModel());
+    console.log('onDidMount', editor, editor.getValue(), editor.getModel());
     this.editor = editor;
   }
   onChange(newValue, e) {
@@ -60,7 +26,7 @@ class CodeEditor extends React.Component {
     this.setState({code: '// code changed by setState! \n'});
   }
   render() {
-    const initialCode = this.state.code;
+    const code = this.state.code;
     const options = {
       selectOnLineNumbers: true,
       roundedSelection: false,
@@ -76,11 +42,10 @@ class CodeEditor extends React.Component {
             <button onClick={::this.changeBySetState}>Change by setState</button>
           </div>
           <hr />
-          <SampleEditor
-              width="800"
-              height="600"
+          <MonacoEditor
+              height="500"
               language="javascript"
-              value={initialCode}
+              value={code}
               options={options}
               onChange={::this.onChange}
               onDidMount={::this.onDidMount}
@@ -99,7 +64,7 @@ class AnotherEditor extends React.Component {
     }
   }
   render() {
-    const initialCode = this.state.code;
+    const code = this.state.code;
     const requireConfig = {
       url: 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.1/require.min.js',
       paths: {
@@ -108,11 +73,11 @@ class AnotherEditor extends React.Component {
     };
     return (
         <div>
-          <SampleEditor
+          <MonacoEditor
               width="800"
               height="600"
               language="javascript"
-              value={initialCode}
+              defaultValue={code}
               requireConfig={requireConfig}
           />
         </div>
@@ -124,9 +89,10 @@ class App extends React.Component {
   render() {
     return (
         <div>
+          <h2>Monaco Editor Sample (controlled mode)</h2>
           <CodeEditor />
           <hr />
-          <h2>Another editor</h2>
+          <h2>Another editor (uncontrolled mode)</h2>
           <AnotherEditor />
         </div>
     );
