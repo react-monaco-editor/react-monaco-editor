0a77d5894c6da07c178b54291b456ddb48d3101e
Daniele Esposti <daniele.esposti@gmail.com>
2017-11-24 23:16:28 +0000

    Upgraded Webpack config

--- examples/browser/webpack.config.js
+++ examples/browser/webpack.config.js
@@ -11,48 +11,33 @@ module.exports = {
     filename: 'index.js',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.html$/,
-        loader: 'file?name=[name].[ext]',
-      },
-      {
-        test: /\.json$/,
-        loader: 'json',
+        use: ['file?name=[name].[ext]'],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loaders: [
-          'react-hot',
-          'babel-loader',
-        ],
+        use: [{ loader: 'react-hot-loader/webpack' }, { loader: 'babel-loader' }]
       },
     ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json'],
-    alias: {
-      'react-monaco-editor': MonacoEditorSrc
-    }
+    extensions: ['.js', '.json'],
+    alias: { 'react-monaco-editor': MonacoEditorSrc }
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+    new webpack.SourceMapDevToolPlugin({ exclude: /node_modules/ }),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') },
     }),
-    new webpack.SourceMapDevToolPlugin({
-      exclude: /node_modules/,
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'node_modules/monaco-editor/min/vs',
-        to: 'vs',
-      }
-    ]),
+    new CopyWebpackPlugin([{
+      from: 'node_modules/monaco-editor/min/vs',
+      to: 'vs',
+    }]),
   ],
-  devServer: {
-    contentBase: './',
-    hot: true,
-  },
+  devServer: { contentBase: './' }
 }
