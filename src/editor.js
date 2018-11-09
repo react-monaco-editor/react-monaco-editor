b2ce53f4a8ff3f5d38bba0d063a0c067dc728ea8
Luke Andrews <luke@attaboy.ca>
2018-11-09 14:04:32 -0500

    Only import the editor API instead of the whole package so that Webpack only includes the most relevant code.

--- src/editor.js
+++ src/editor.js
@@ -1,4 +1,4 @@
-import * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { processSize } from './utils'
