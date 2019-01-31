d50d31ad8360e9947d4f9dd22f52f9651a67f9d0
David Gomes <davidrafagomes@gmail.com>
2019-01-31 19:37:33 +0000

    Adds missing property in the component instance for React refs. (#174)
    When creating a ref of the `MonacoEditor`, we need the `editor` property in the component's instance in order for the ref to work.

--- src/index.d.ts
+++ src/index.d.ts
@@ -80,7 +80,9 @@ export interface MonacoEditorProps extends MonacoEditorBaseProps {
   onChange?: ChangeHandler;
 }
 
-export default class MonacoEditor extends React.Component<MonacoEditorProps> {}
+export default class MonacoEditor extends React.Component<MonacoEditorProps> {
+  editor?: monacoEditor.editor.IStandaloneCodeEditor;
+}
 
 // ============ Diff Editor ============
 
@@ -126,4 +128,6 @@ export interface MonacoDiffEditorProps extends MonacoEditorBaseProps {
   onChange?: DiffChangeHandler;
 }
 
-export class MonacoDiffEditor extends React.Component<MonacoDiffEditorProps> {}
+export class MonacoDiffEditor extends React.Component<MonacoDiffEditorProps> {
+  editor?: monacoEditor.editor.IStandaloneDiffEditor;
+}
