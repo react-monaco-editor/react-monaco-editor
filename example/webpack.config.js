25f79e0fbf10cb002da7e251ae2900e4e95db370
Dominik Moritz <domoritz@gmail.com>
2019-07-20 18:49:35 +0200

    Update babel

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -20,7 +20,18 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [{ loader: 'babel-loader' }]
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+            ]
+          }
+        }]
       },
       {
         test: /\.css$/,
