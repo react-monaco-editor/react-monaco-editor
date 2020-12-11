182c7f003acf61ba6279f20a5aebf1dfe5e032cf
Miriam Soloveichik <65854327+miriamso@users.noreply.github.com>
2020-12-11 17:10:16 +0200

    Export monaco-editor types for typescript (#315)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import MonacoEditor from "./editor";
 import MonacoDiffEditor from "./diff";
 
+export * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 export * from "./types";
 export { MonacoEditor as default, MonacoDiffEditor };
