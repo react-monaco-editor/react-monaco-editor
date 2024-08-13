218893e15737b8863e61bb12b00cf6186bc80b27
Amrit Kashyap Borah <66767648+Elessar1802@users.noreply.github.com>
2024-08-14 04:30:35 +0530

    fix: stale onChange callback in onDidChangeModelContent handler (#955)

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,11 @@
+import { MonacoEditorProps } from "./types";
+
 export function processSize(size: number | string) {
   return !/^\d+$/.test(size as string) ? size : `${size}px`;
 }
 
 export function noop() {}
+
+export const propCallbacks: Pick<MonacoEditorProps, "onChange"> = {
+  onChange: null,
+};
