32c57751cb3c4e5ca749980279d98051148ad0fb
Amrit Kashyap Borah <66767648+Elessar1802@users.noreply.github.com>
2024-10-04 19:19:58 +0530

    fix: using wrong onChange if multiple instances of monaco editor are mounted on the same screen (#976)

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,5 @@
-import { MonacoEditorProps } from "./types";
-
 export function processSize(size: number | string) {
   return !/^\d+$/.test(size as string) ? size : `${size}px`;
 }
 
 export function noop() {}
-
-export const propCallbacks: Pick<MonacoEditorProps, "onChange"> = {
-  onChange: null,
-};
